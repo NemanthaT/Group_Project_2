a4e48790f5003b8ccce6bfc5fdac9e2f2a5ff2ba
Rehan Peiris <rehanperis58@gmail.com>
2025-10-17 03:44:58 +0530

    Small fix to eventRoutes

--- Will_Fair/server/routes/eventRoutes.js
+++ Will_Fair/server/routes/eventRoutes.js
@@ -1,15 +1,15 @@
 import express from "express";
-import upload from "../middleware/upload.js";
-import { getEventsController, createEvent } from "../controllers/EventController.js";
+//import upload from "../middleware/upload.js";
+import { getEventsController } from "../controllers/eventController.js";
 
 const router = express.Router();
 
 //Controlling file uploading and image uploading for event creation
-router.post('/createEvent', upload.fields([{
+/* router.post('/createEvent', upload.fields([{
   name: 'image', maxCount: 1
 }, {
   name: 'documents', maxCount: 5
-}]), createEvent);
+}]), createEvent); */
 
 //public Routes
 router.get('/', getEventsController);
